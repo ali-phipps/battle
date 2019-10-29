e8b255b4004f1f494c1812333c88f1e1683017f2
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-10-29 16:25:42 +0000

    sessions added and /play

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     "Testing infrastructure working!"
@@ -13,8 +14,16 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @Player_1 = params[:Player_1]
-    @Player_2 = params[:Player_2]
+    session['Player_1'] = params[:Player_1]
+    session['Player_2'] = params[:Player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    p session
+    @Player_1 = session['Player_1']
+    @Player_2 = session['Player_2']
+
     erb(:play)
   end
   # start the server if ruby file executed directly
